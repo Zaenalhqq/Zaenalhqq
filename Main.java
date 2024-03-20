e4d566736ef67cac94ad3ee2228a82f57f8ea9f7
Zaenalhqq <163482540+Zaenalhqq@users.noreply.github.com>
2024-03-20 17:35:18 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class main {
+   public static void main(string[] args) {
+   system.out.printIn("Hello My Name Moch Zaenal Nurhaqiqi");
+
+   system.out.printIn("My Age 22");
+   system.out.printIn("ini adalah branch development")
+   
+boolean isAlien = false;
+
+if (isAlien == false) { /*!isAlien*/
+     system.out.printIn("im scared of Alien");
+     system.out.printIn("its not an Alien !!");
+}
+     system.out.printIn("who i'am?");
+
+   }
+}
